07337bb5f0810f41b1f4f092beb17c5a61a20b4c
cpprhtn <cpprhtn@naver.com>
2024-01-14 20:11:17 +0900

    test: Rust wasm test

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,160 @@
+<!-- <script lang='ts'>
+
+</script>
+<style>
+	main {
+	  max-width: 800px;
+	  margin: 0 auto;
+	  padding: 20px;
+	  text-align: center;
+	}
+  
+	h1 {
+	  font-size: 2.5em;
+	  margin-bottom: 20px;
+	}
+  
+	h2 {
+	  font-size: 1.5em;
+	  margin-bottom: 10px;
+	}
+  
+	.LinkButton {
+	  margin-top: 20px;
+	  color: var(--text-3);
+	}
+  
+	.button-container {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  background-color: #f9f9f9;
+	  padding: 20px;
+	  border-radius: 8px;
+	  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	  transition: box-shadow 0.3s ease;
+	  width: auto;
+	}
+  
+	a {
+	  text-decoration: none;
+	  color: #333;
+	}
+  
+	.shadow-button:hover {
+	  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+	}
+  
+	.button_Description {
+	  margin-top: 5px;
+	  font-size: 0.9em;
+	  color: #777;
+	}
+  
+	svg {
+	  margin-bottom: 10px;
+	}
+  </style>
+  
+  <main>
+	<h1>"끊임없이 공부하고 끊임없이 노력합니다."</h1>
+	<h2>C++ Korea는 2013년 11월에 설립된 그룹으로<br>C++의 저변확대를 위한 보급에 힘쓰는 단체입니다.</h2>
+	<h2>C++ Korea는 Meta(구 Facebook)에서 2013년 11월에 설립된 이래 비영리 단체로의 도약을 준비하고 있습니다.</h2>
+  
+	<div class="LinkButton">
+	  <a href="https://www.facebook.com/groups/cppkorea/" target="_blank" class="col-sm shadow-button">
+		<div class="button-container">
+		  <svg aria-hidden="true" data-prefix="fab" data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon svg-inline--fa fa-facebook fa-w-14" width="50px" height="49px" style="color: #3b5998;">
+			<path fill="currentColor" d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z" class="">
+			</path>
+		  </svg>
+		  <h2>C++ Korea 그룹으로 이동</h2>
+		  <p class="button_Description">C++ Korea Meta(facebook) 그룹입니다</p>
+		  <p class="button_Description">질문 및 답변 등을 할 수 있습니다.</p>
+		  facebook.com/groups/cppkorea
+		</div>
+	  </a>
+	</div>
+  
+	<div class="LinkButton">
+	  <a href="https://github.com/cppkorea" target="_blank" class="col-sm shadow-button">
+		<div class="button-container">
+		  <svg aria-hidden="true" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="icon svg-inline--fa fa-github fa-w-16" width="50px" height="49px" color="#000">
+			<path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" class="">
+			</path>
+		  </svg>
+		  <h2>C++ Korea Github로 이동</h2>
+		  <p class="button_Description">C++ Korea에서 진행한 세미나, 스터디,</p>
+		  <p class="button_Description">핵심 가이드라인이 저장된 Git 저장소입니다.</p>
+		  <p class="button_Description">github.com/cppkorea</p>
+		</div>
+	  </a>
+	</div>
+  </main> -->
+
+
+<script>
+	import { onMount } from 'svelte';
+	import init, { compile_cpp } from '../../../../wasm_cpp_compiler/pkg/wasm_cpp_compiler';
+  
+	let cppCode = '';
+	let result = '';
+  
+	onMount(async () => {
+	  await init(); // Initialize the WASM module
+	});
+  
+	const compileCPlusPlus = async () => {
+	  result = compile_cpp(cppCode);
+	};
+  </script>
+  
+  <main>
+	<h1>C++ Web Compiler</h1>
+	<textarea bind:value={cppCode} placeholder="Enter your C++ code"></textarea>
+	<button on:click={compileCPlusPlus}>Compile</button>
+	{#if result}
+	  <div>
+		<h2>Result:</h2>
+		<pre>{result}</pre>
+	  </div>
+	{/if}
+  </main>
+  
+  <style>
+	main {
+	  max-width: 600px;
+	  margin: 0 auto;
+	  text-align: center;
+	  padding: 20px;
+	}
+  
+	h1 {
+	  font-size: 2em;
+	  margin-bottom: 20px;
+	}
+  
+	textarea {
+	  width: 100%;
+	  height: 200px;
+	  margin-bottom: 10px;
+	}
+  
+	button {
+	  padding: 10px 20px;
+	  font-size: 1em;
+	  background-color: #4caf50;
+	  color: #fff;
+	  cursor: pointer;
+	  border: none;
+	  border-radius: 4px;
+	}
+  
+	div {
+	  margin-top: 20px;
+	}
+  
+	pre {
+	  white-space: pre-wrap;
+	}
+  </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default {
+	plugins: [sveltekit()],
+	server: {
+	  fs: {
+		strict: false,
+	  },
+	  proxy: {
+		'/pkg': 'http://localhost:5000/pkg',
+	  },
+	},
+  };
+
+// export default defineConfig({
+// 	plugins: [sveltekit()]
+// });
