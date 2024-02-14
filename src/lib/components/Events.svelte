943d08aadb4e5a5f77a4c24485215b4927809993
cpprhtn <cpprhtn@naver.com>
2024-02-14 20:04:18 +0900

    Update Events.svelte

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -1,17 +1,38 @@
-<script lang='ts'>
-
-</script>
-<style>
+<script>
+	import { onMount } from "svelte";
+  
+	let linkButtons = [];
+  
+	onMount(() => {
+	  // event.txt 파일 읽기
+	  fetch('event.txt')
+		.then(response => response.text())
+		.then(data => {
+		  // 개행 문자를 기준으로 데이터 분리
+		  const lines = data.split('\n');
+  
+		  // 각 라인의 데이터 처리
+		  linkButtons = lines.map(line => {
+			// 데이터를 콤마로 분리
+			const [url, image, description] = line.split(',');
+  
+			return {
+			  url,
+			  image,
+			  description
+			};
+		  });
+		})
+		.catch(error => console.error('Error reading event.txt:', error));
+	});
+  </script>
+  
+  <style>
 	main {
 	  max-width: 800px;
 	  margin: 0 auto;
 	  padding: 20px;
 	  text-align: center;
-	}
-  
-	h1 {
-	  font-size: 2.5em;
-	  margin-bottom: 20px;
 	}
   
 	h2 {
@@ -20,9 +41,10 @@
 	}
   
 	.LinkButton {
-	  margin-top: 20px;
+	  margin-top: 40px;
 	  color: var(--text-3);
 	  height: 400px;
+	  align-items: center;
 	}
   
 	.button-container {
@@ -34,11 +56,10 @@
 	  border-radius: 8px;
 	  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 	  transition: box-shadow 0.3s ease;
-	  width: auto;
 	}
-
+  
 	p {
-		color: var(--text-1);
+	  color: var(--text-1);
 	}
   
 	a {
@@ -49,37 +70,19 @@
 	.shadow-button:hover {
 	  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
 	}
-  
-	.button_Description {
-	  margin-top: 5px;
-	  font-size: 0.9em;
-	  color: #777;
-	}
-  
-	svg {
-	  margin-bottom: 10px;
-	}
   </style>
   
   <main>
 	<h2>"다양한 분야에서 전문 개발자를 위해<br>정기적으로 밋업과 기술 세미나를 운영합니다."</h2>
-
-	<div class="LinkButton">
-		<a href="https://festa.io/events/4635" target="_blank" class="col-sm shadow-button">
-		  <!-- 이미지 외부경로 불러오기 -->
-		  <div class="button-container" style="height: 380px; background-image: url('/cppstudy1.png'); background-size: cover; background-position: center;">
+  
+	{#each linkButtons as {url, image, description}}
+	  <div class="LinkButton">
+		<a href={url} target="_blank" class="col-sm shadow-button">
+		  <div class="button-container" style="height: 380px; background-image: url('{image}'); background-size: cover; background-position: center;">
 		  </div>
 		</a>
-		<p>리눅스 환경(WSL 포함)에서 C++ 실습하기</p>
-	</div>
-
-	<div class="LinkButton">
-		<a href="https://festa.io/events/4635" target="_blank" class="col-sm shadow-button">
-		  <!-- 이미지 외부경로 불러오기 -->
-		  <div class="button-container" style="height: 380px; background-image: url('/coreguidelines.png'); background-size: cover; background-position: center;">
-			
-		  </div>
-		</a>
-		<p>2024년 C++ 핵심 가이드라인 스터디 첫 모임</p>
-	</div>
+		<p>{description}</p>
+	  </div>
+	{/each}
   </main>
+  
